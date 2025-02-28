f1d3091d47ca6948b4a2d30046405abe0d9072df
nooseAPI <dongyeob0518@naver.com>
2025-03-01 01:28:14 +0900

    Modify readme url

--- src/store/about.js
+++ src/store/about.js
@@ -2,15 +2,9 @@ export default {
   namespaced: true,
   state: () => ({
     name: 'nooseAPI',
-    //email: 'thesecon@gmail.com',
     email: 'dongyeob0518@naver.com',
-    //blog: 'https://nooseAPI.blog',
     blog: 'https://blog.naver.com/dongyeob0518',
     phone: '+82-10-1234-5678',
     image : 'favicon.png'
-    //image : src="require(`@/static/favicon.png`)" 
-    //image: 'https://heropy.blog/css/images/logo.png'
-    
-    
   })
 }
