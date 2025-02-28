43f7033cd90895c4079fd82770ee430e164a7070
nooseAPI <dongyeob0518@naver.com>
2025-02-28 18:38:24 +0900

    Modify logo.png

--- src/store/about.js
+++ src/store/about.js
@@ -7,7 +7,7 @@ export default {
     //blog: 'https://nooseAPI.blog',
     blog: 'https://blog.naver.com/dongyeob0518',
     phone: '+82-10-1234-5678',
-    image : 'static/favicon.png'
+    image : 'favicon.png'
     //image : src="require(`@/static/favicon.png`)" 
     //image: 'https://heropy.blog/css/images/logo.png'
     
